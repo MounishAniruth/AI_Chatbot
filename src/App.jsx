070a533a483618214bdaa7bcc044ef38ac6afbe7
T.Mounish Aniruth <Mounishaniruth1708@gmail.com>
2026-08-18 11:37:19 +0530

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import JobApplicationPage from "./pages/JobApplicationPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Job Application Page */}
+        <Route
+          path="/job-application"
+          element={<JobApplicationPage />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
