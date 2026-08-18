c9a764786a7a6ca13cfe70d099e82712bae65718
T.Mounish Aniruth <Mounishaniruth1708@gmail.com>
2026-08-18 12:20:11 +0530

    Setup Vite React project and landing page

---
+++ src/pages/JobApplicationPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JobApplicationPage = () => {
+  return (
+    <div>
+      <h1>Job Application</h1>
+      <p>Job application page coming soon.</p>
+    </div>
+  );
+};
+
+export default JobApplicationPage;
